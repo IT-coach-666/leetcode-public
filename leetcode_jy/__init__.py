dd5284c34892664044928c03a6a8f8d2c2b38f2a
IT-coach-666 <929277335@qq.com>
2023-11-12 10:31:28 +0800

    初始化项目结构

---
+++ leetcode_jy/__init__.py
@@ -0,0 +1,5 @@
+
+version = "0.0.1"
+print("leetcode_jy: version-%s 【语雀知识库: www.yuque.com/it-coach】" % version)
+
+
---
+++ leetcode_jy/jy_tools.py
@@ -0,0 +1,46 @@
+
+"""
+该脚本功能:
+1) 创建该项目的目录结构
+2) 统计汇总各题目的属性(难度系数、题目类型)
+
+"""
+
+import os
+
+def num_format(num):
+    return "{0:0>4}".format(num)
+
+def generate_dir_file():
+    max_num = 2500
+    first_step = 500
+    sec_step = 50
+    for i in range(0, max_num, first_step):
+        dir_name = "jy_%s_%s" % (num_format(i+1), num_format(i + first_step))
+        #print(dir_name)
+        if not os.path.isdir(dir_name):
+            print("创建一级目录: %s" % dir_name)
+            os.mkdir(dir_name)
+
+        for j in range(i, i + first_step, sec_step):
+            if j > max_num:
+                break
+            sec_dir_name = "%s/jy_%s_%s" % (dir_name, num_format(j+1),
+                                            num_format(j + sec_step))
+            #print(sec_dir_name) 
+            if not os.path.isdir(sec_dir_name):
+                print("创建二级目录: %s" % sec_dir_name)
+                os.mkdir(sec_dir_name)
+            #else:
+            #    print("删除已存在的目录: %s" % sec_dir_name)
+            #    os.rmdir(sec_dir_name)
+            for k in range(j, j + sec_step):
+                f_name = "%s/jy_%s.py" % (sec_dir_name, num_format(k+1))
+                #print(f_name)
+                if not os.path.isfile(f_name):
+                    print("创建空文件: %s" % f_name)
+                    os.mknod(f_name)
+
+generate_dir_file()
+
+
