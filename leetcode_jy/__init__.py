4f655a4964d378445cf535805610f2af66fc5fe2
IT-coach-666 <929277335@qq.com>
2023-11-16 15:41:09 +0800

    update 3-question

--- leetcode_jy/__init__.py
+++ leetcode_jy/__init__.py
@@ -20,9 +20,35 @@ from leetcode_jy import *
 """
 
 import os
+import io
+
 
 def num_format(num):
     return "{0:0>4}".format(num)
+
+
+def get_statistic(f_name):
+    """
+    传入参数:
+    f_name: 如 "jy_0001_0500/jy_0001_0050/jy_0001.py"
+    """
+    split_ = f_name.split("/")
+    obj_num = split_[-1].lstrip("jy_").rstrip(".py")
+
+    type_jy, title_jy, q_type, tag_jy = None, None, None, None
+    with io.open(f_name) as f_:
+        for line in f_:
+            if line.startswith("type_jy"):
+                type_jy = line.split("=")[-1].strip("\"\' \n")
+            if line.startswith("title_jy"):
+                tmp_ = line.split("=")[-1].strip().split("(")
+                title_jy = tmp_[0].strip("\"\'")
+                q_type = "{0:<11}".format(tmp_[1].strip("\"\')"))
+            if line.startswith("tag_jy"):
+                tag_jy = line.split("=")[-1].strip("\"\' \n")
+            if type_jy and title_jy and q_type and tag_jy:
+                return [obj_num, type_jy, q_type, title_jy, tag_jy]
+    return []
 
 def generate_dir_file(is_statistic=False):
     """
@@ -34,16 +60,20 @@ def generate_dir_file(is_statistic=False):
     max_num = 2500
     first_step = 500
     sec_step = 50
+    # jy: 一级目录
     for i in range(0, max_num, first_step):
+        # jy: 一级目录名
         dir_name = "jy_%s_%s" % (num_format(i+1), num_format(i + first_step))
         #print(dir_name)
         if not os.path.isdir(dir_name):
             print("创建一级目录: %s" % dir_name)
             os.mkdir(dir_name)
 
+        # jy: 二级目录
         for j in range(i, i + first_step, sec_step):
             if j > max_num:
                 break
+            # jy: 二级目录名
             sec_dir_name = "%s/jy_%s_%s" % (dir_name, num_format(j+1),
                                             num_format(j + sec_step))
             #print(sec_dir_name) 
@@ -53,12 +83,24 @@ def generate_dir_file(is_statistic=False):
             #else:
             #    print("删除已存在的目录: %s" % sec_dir_name)
             #    os.rmdir(sec_dir_name)
+
+            # jy: 三级目录(对应具体文件名)
             for k in range(j, j + sec_step):
+                # jy: 文件名
                 f_name = "%s/jy_%s.py" % (sec_dir_name, num_format(k+1))
                 #print(f_name)
                 if not os.path.isfile(f_name):
                     print("创建空文件: %s" % f_name)
                     os.mknod(f_name)
+                # jy: 读取文件中的 "type_jy" 和 "title_jy" 变量值
+                if is_statistic:
+                    ls_info = get_statistic(f_name)
+                    if ls_info:
+                        print(ls_info)
 
-#generate_dir_file()
+if __name__ == "__main__":
+    #generate_dir_file()
+    generate_dir_file(is_statistic=True)
+
+
 
--- leetcode_jy/jy_0001_0500/jy_0001_0050/jy_0001.py
+++ leetcode_jy/jy_0001_0500/jy_0001_0050/jy_0001.py
@@ -6,15 +6,19 @@ abs_path = os.path.abspath(__file__)
 dir_project = os.path.join(abs_path.split("leetcode_jy")[0], "leetcode_jy")
 sys.path.append(dir_project)
 from leetcode_jy import *
-
-#from leetcode_jy.jy_utils import test_name
-#print("============= ", test_name)
+# jy: 记录该题的难度系数
+type_jy = "S"
+# jy: 记录该题的英文简称以及所属类别
+title_jy = "Two-Sum(array_dim_1)"
+# jy: 记录不同解法思路的关键词
+tag_jy = "巧用字典"
 
 
 """
-Given an array of integers, return indices of the two numbers such that they 
-add up to a specific target.  You may assume that each input would have exactly 
-one solution, and you may not use the same element twice.
+Given an array of integers, return indices of the two
+numbers such that they add up to a specific target.  You
+may assume that each input would have exactly one solution,
+and you may not use the same element twice.
 
 
 Example:
@@ -29,82 +33,75 @@ from typing import List
 
 class Solution:
     """
-解法1: 假设 n + m = target, 如果 n 在数组中, 且 m 也在数组中, 则有对应的一组数满足条件;
+解法1: 假设 n + m = target, 如果 n 在数组中, 且 m 也在数组中, 则
+有对应的一组数满足条件
 
-因此可以遍历数组中的每一个元素 n, 判断 ``target - n`` 是否在数组中, 如果存在则返回
-``target - n`` 与 n 的下标;
+因此可以遍历数组中的每一个元素 n, 判断 ``target - n`` 是否在数组中,
+如果存在则返回 ``target - n`` 与 n 的下标
 
-算法的复杂度: ``f(n) * O(n)``, 其中 f(n) 为判断 ``target - n`` 是否在数组中的复杂度; 
-为了降低整体的复杂度, f(n) 最好是 O(lg(n)) 甚至 O(1); 哈希表获取元素的复杂度是 O(1), 因
-此使用哈希表可以将整个算法的复杂度可以降为 O(n);
+哈希表获取元素的复杂度是 O(1), 算法整体的复杂度为 O(n)
+
     """
-    def twoSum_v1(self, nums: List[int], target: int) -> List[int]:
+    def twoSum_v1(self, nums: List[int],
+                  target: int) -> List[int]:
         mapping = {}
         for index, num in enumerate(nums):
             if target - num in mapping:
-                # jy: 如果有多组满足条件的组合, 则可以定义一个列表存放相应的组合, 最后再
-                #     返回相应列表(此处的 return 需要修改)
+                # jy: 如果有多组满足条件的组合, 则可以定义一个列表
+                #     存放相应的组合, 最后再返回相应列表(此处的
+                #     return 需要修改)
                 return [mapping[target - num], index]
-            # jy: 以下 else 语句可以简化: 由于以上的 if 语句会直接 return, 故以下没必要
-            #     有 else 逻辑存在, 直接 ``mapping[num] = index`` 即可(需回退一次原先
-            #     的缩进);
+            # jy: 以下 else 语句可以简化: 由于以上的 if 语句会直
+            #     接 return, 故以下没必要有 else 逻辑存在, 直
+            #     接 ``mapping[num] = index`` 即可(需回退一次
+            #     原先的缩进)
             else:
                 mapping[num] = index
-        # jy: 如果不存在满足要求的两个数, 则返回 [-1, -1] (如果确定是存在的, 则这部分代
-        #     码可省略)
+        # jy: 如果不存在满足要求的两个数, 则返回 [-1, -1] (如果确
+        #     定是存在的, 则这部分代码可省略)
         return [-1, -1]
 
     """
 解法2: 不使用字典的方式(性能下降, 非空间换时间)
     """
-    def twoSum_v2(self, nums: List[int], target: int) -> List[int]:
+    def twoSum_v2(self, nums: List[int],
+                  target: int) -> List[int]:
         for idx, num in enumerate(nums):
             if target - num in nums and nums.index(target - num) != idx:
                 return [idx, nums.index(target - num)]
-
+                
     """
-题目变换: 找出 nums 列表中所有和为 target 的二元组组合 (要求组合不重复)
+找出 nums 列表中所有和为 target 的二元组组合 (要求组合中同一
+个数值不重复使用)
 
-以下解法复杂化了, 更优解思路同解法 1: 在解法 1 的基础上, 碰到符合要求的一对数
-后将其加入二元组列表, 并将对应的数值加入集合(该集合可用于限制同样的一组数重复出
-现, 如果没有该限制, 可以不使用该集合), 为了防止数值的重复使用, 再将原先添加入
-到 mapping 字典的数值剔除; 直到遍历完整个数组为止;
+在解法 1 的基础上, 碰到符合要求的一对数后将其加入列表, 并将其从
+mapping 中剔除, 防止重复使用
+
+如果进一步要求符合要求的一对数不能相同, 则可进一步定义一个集合,
+用于存储满足要求的一对数, 后续再次碰到满足要求的一对数的其中一个
+数时, 只需判断该数是否在该集合中, 如果已经在该集合中, 表明基于该
+数构成的一对满足要求的数已经存在, 直接跳过即可
     """
-    def twoSum_v3(self, nums, target):
-        # jy: 统计数值以及其出现的次数;
-        dict_num_count = {}
-        dict_pair_count = {}
-        is_visited = set()
-        for num in nums:
-            if num in dict_num_count:
-                dict_num_count[num] += 1
+    def twoSum_v3(self, nums: List[int], target: int,
+                  deduplicate=True) -> List[int]:
+        mapping = {}
+        ls_res = []
+        # jy: 用于记录已经确认过满足要求的一对数(记录之后, 后续方便去重)
+        set_done = set()
+        for index, num in enumerate(nums):
+            #if target - num in mapping:
+            if target - num in mapping and target - num not in set_done:
+                # jy: 将目标下标值存储到 ls_res, 并从 mapping 中剔除
+                #     防止相同数值后续重复使用
+                ls_res.append([mapping.pop(target - num), index])
+                # jy: 如果要求去重, 则已经得到过的一对数要添加
+                #     到 set_done 集合中
+                if deduplicate:
+                    set_done.add(target - num)
+                    set_done.add(num)
             else:
-                dict_num_count[num] = 1
-
-        for i, count in dict_num_count.items():
-            j = target - i
-            if j in dict_num_count and j not in is_visited:
-                if i == j:
-                    if dict_num_count[i] >= 2:
-                        dict_pair_count[(i, j)] = dict_num_count[i] // 2
-                else:
-                    dict_pair_count[(i, j)] = min(dict_num_count[i], 
-                                                  dict_num_count[j])
-            # jy: 以下代码可以再缩进, 不会产生影响;
-            is_visited.add(i)
-            is_visited.add(j)
-        return dict_pair_count
-
-    def twoSum_2022_02_27(self, nums: List[int], target: int) -> List[int]:
-        dict_ = {}
-        for idx, num in enumerate(nums):
-            if target - num in dict_:
-                return [idx, dict_[target - num]]
-            # jy: 以下 else 语句可以简化: 由于以上的 if 语句会直接 return, 故以下的没
-            #     必要有 else 逻辑存在, 直接 ``dict_[num] = idx`` 即可(需回退一次原先
-            #     的缩进);
-            else:
-                dict_[num] = idx
+                mapping[num] = index
+        return ls_res
 
 
 nums = [2, 7, 11, 15]
@@ -112,21 +109,31 @@ target = 9
 res = Solution().twoSum_v1(nums, target)
 print(res)
 
-
 nums = [1, 8, 2, 7, 11, 15]
 target = 9
 res = Solution().twoSum_v2(nums, target)
 print(res)
 
-
 nums = [1, 8, 2, 7, 11, 5, 4, 5, 4, 15, 9, 0, 9, 0]
 target = 9
 res = Solution().twoSum_v3(nums, target)
+# jy: [[0, 1], [2, 3], [5, 6], [10, 11]]
 print(res)
+
+res = Solution().twoSum_v3(nums, target, False)
+# jy: [[0, 1], [2, 3], [5, 6], [7, 8], [10, 11], [12, 13]]
+print(res)
+
 
 nums = [1, 5, 2, 4, 3, 3, 3, 5, 4, 3, 3]
 target = 6
 res = Solution().twoSum_v3(nums, target)
+# jy: [[0, 1], [2, 3], [4, 5]]
+print(res)
+
+
+res = Solution().twoSum_v3(nums, target, False)
+# jy: [[0, 1], [2, 3], [4, 5], [6, 9]]
 print(res)
 
 
