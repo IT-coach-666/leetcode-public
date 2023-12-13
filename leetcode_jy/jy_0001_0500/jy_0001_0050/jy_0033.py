ec2238a9d5143915ecdd14f42dba08a9fa9dad20
IT-coach-666 <929277335@qq.com>
2023-12-13 13:03:48 +0800

    0033

--- leetcode_jy/jy_0001_0500/jy_0001_0050/jy_0033.py
+++ leetcode_jy/jy_0001_0500/jy_0001_0050/jy_0033.py
@@ -14,19 +14,18 @@ type_jy = "M"
 # jy: 记录该题的英文简称以及所属类别
 title_jy = "search-in-rotated-sorted-array(array_dim_1)"
 # jy: 记录不同解法思路的关键词
-tag_jy = ""
+tag_jy = "二分查找 | 相似题: 0081 (数值可重复出现)"
 
 
 
 """
-Suppose an array sorted in ascending order is rotated at some pivot unknown
-to you beforehand. i.e.: [0, 1, 2, 4, 5, 6, 7] might become [4, 5, 6, 7, 0, 1, 2]
+Suppose an array sorted in ascending order is rotated at some pivot unknown to
+you beforehand. i.e.: [0, 1, 2, 4, 5, 6, 7] might become [4, 5, 6, 7, 0, 1, 2]
 
-You are given a target value to search. If found in the array return its
-index, otherwise return -1.
+You are given a target value to search. If found in the array return its index,
+otherwise return -1. You may assume no duplicate exists in the array. Your 
+algorithm's runtime complexity must be in the order of O(log n).
 
-You may assume no duplicate exists in the array.
-Your algorithm's runtime complexity must be in the order of O(log n).
 
 Example 1:
 Input: nums = [4, 5, 6, 7, 0, 1, 2], target = 0
@@ -42,128 +41,130 @@ Output: 1
 """
 
 
-from typing import List
 class Solution:
     """
-二分查找:
-如果是一个普通有序数组(如升序), 只需要 middle 与 target 之间的简单判断:
-(1) 如果 nums[middle] == target, 则直接返回 middle 值;
-(2) 如果 nums[middle] < target, 则 target 在 middle 的后半部分, low = middle+1
-(3) 如果 nums[middle] > target, 则 target 在 middle 的前半部分, high = middle-1
+解法 1: 二分查找
 
-如果是一个旋转有序数组(升序), 则需要在 middle 和 target 的基础上再结合 high 进行判断:
-(1) 如果 nums[middle] == target, 则直接返回 middle 值; 否则根据 middle target high 的情况进行分类;
-(2) 如果 nums[middle] > target, 根据 nums[high] 所处位置不同可分以下 3 种情况:
-    1) target < nums[middle] < nums[high] , 说明 target 在旋转数组的前半部分, high 移动到 middle-1
-    2) nums[high] < target < nums[middle] , 说明 target 在旋转数组的前半部分, high 移动到 middle-1
-    3) target < nums[high] < nums[middle] , 说明 target 在旋转数组的后半部分, low 移动到 middle+1
+如果是一个普通升序数组, 只需要 mid 与 target 之间的简单判断:
+a) 如果 nums[mid] == target, 则直接返回 mid 值
+b) 如果 nums[mid] < target, 则 target 在 mid 的后半部分, low = mid+1
+c) 如果 nums[mid] > target, 则 target 在 mid 的前半部分, high = mid-1
 
-(3) 如果 nums[middle] < target, 根据 nums[high] 所处位置不同可分 3 种情况: 同理如上
+旋转升序数组从任意位置劈开后, 至少有一半是有序的, 因此可以先找到有序的一段, 然
+后看 target 在不在这一段里, 如果在, 就把另一半丢弃; 如果不在, 就把这一段丢弃
+
+在 nums[mid] 和 target 的基础上再结合 nums[high] 进行判断:
+a) 如果 nums[mid] == target, 则直接返回 mid 值
+b) 如果 nums[mid] > target, 根据 nums[high] 所处位置不同可分以下 3 种情况:
+   1) target < nums[mid] < nums[high], 说明 target 在旋转数组的前半部分, high 移动到 mid-1
+   2) nums[high] < target < nums[mid], 说明 target 在旋转数组的前半部分, high 移动到 mid-1
+   3) target < nums[high] < nums[mid], 说明 target 在旋转数组的后半部分, low 移动到 mid+1
+c) 如果 nums[mid] < target, 根据 nums[high] 所处位置不同可分 3 种情况: 
+   1) target > nums[mid] > nums[high], 说明 target 在旋转数组的后半部分, low 移动到 mid+1
+   2) nums[high] > target > nums[mid], 说明 target 在旋转数组的后半部分, low 移动到 mid+1
+   3) target > nums[high] > nums[mid], 说明 target 在旋转数组的前半部分, high 移动到 mid-1
     """
-    def search(self, nums: List[int], target: int) -> int:
+    def search_v1(self, nums: List[int], target: int) -> int:
         low, high = 0, len(nums) - 1
         while low <= high:
-            # jy: 注意 middle 值的计算方式;
-            middle = low + (high - low) // 2
-            # jy: 如果 nums[middle] == target, 则直接返回 middle 值;
-            if nums[middle] == target:
-                return middle
+            # jy: 注意 mid 值的计算方式
+            mid = low + (high - low) // 2
+            # jy: 如果 nums[mid] == target, 则直接返回 mid 值
+            if nums[mid] == target:
+                return mid
 
-            # jy: 如果 nums[middle] > target, 当
-            elif nums[middle] > target:
-                if nums[middle] < nums[high] or nums[high] < target:
-                    high = middle - 1
+            # jy: 优先判断 nums[mid] 与 target 的值的大小
+            #     如果 nums[mid] > target, 则需根据 nums[high] 所处位置判
+            #     断 target 所在的区间
+            if nums[mid] > target:
+                if nums[mid] < nums[high] or nums[high] < target:
+                    high = mid - 1
                 else:
-                    low = middle + 1
-            # jy: nums[middle] < target 时对应的逻辑;
+                    low = mid + 1
+            # jy: nums[mid] < target 时对应的逻辑, 仍需根据 nums[high] 所
+            #     处位置判断 target 所在的区间
             else:
-                if nums[middle] >= nums[high] or nums[high] >= target:
-                    low = middle + 1
+                if nums[mid] >= nums[high] or nums[high] >= target:
+                    low = mid + 1
                 else:
-                    high = middle - 1
+                    high = mid - 1
         return -1
 
 
     """
-解法2(jy): 同样基于 middle  target  high 的不同情况进行分类处理, 只是改写了以上过程, 优先
-判断 middle 和 high, 更易于理解;
-
-还可以基于 middle  target  low 的不同情况进行分类处理, 见 081_Search-in-Rotated-Sorted-Array-II.py
+解法 2: 同解法 1, 区别在于: 解法 1 中优先判断 nums[mid] 与 target 的大小,
+当前解法优先判断 nums[mid] 和 nums[high] 的大小, 以确定哪一半是升序区间,
+并判断 target 是否在该区间中, 从而进一步判断是更新 low 还是 high
     """
-    def search_jy(self, nums: List[int], target: int) -> int:
+    def search_v2(self, nums: List[int], target: int) -> int:
         low, high = 0, len(nums) - 1
         while low <= high:
-            middle = low + (high - low) // 2
-            if nums[middle] == target:
-                return middle
-            # jy: 如果 middle 小于 high, 表明后半部分严格升序; 此时判断 target 是否位于严格
-            #    升序部分所对应区间即可相应的判断并更新 low 和 high 值;
-            #if nums[middle] < nums[high]:
-            elif nums[middle] < nums[high]:  # jy: 此处为 if 或 elif 均可;
-                # jy: 可能存在 target == nums[high], 此部分逻辑要更新 low, 即使
-                #    当 target == nums[high] 成立时也更新 low 也不会错过正确值;
-                if nums[middle] < target <= nums[high]:
-                    low = middle + 1
-                else:
-                    high = middle - 1
-            # jy: 如果 middle 大于 high, 表明前半部分严格升序; 此时判断 target 是否位于严格
-            #    升序部分所对应区间即可相应的判断并更新 low 和 high 值;
-            elif nums[middle] > nums[high]:
-                if nums[low] <= target < nums[middle]:
-                    high = middle - 1
-                else:
-                    low = middle + 1
-            # jy: 此处逻辑即 nums[middle] == nums[high], 只有当有序 pivot 数组中允许
-            #    数值重复才有可能出现该情况(如: 081_Search-in-Rotated-Sorted-Array-II.py);
-            else:
-                #high -= 1  # jy: 模仿 081
-                high = middle - 1
+            mid = low + (high - low) // 2
+            # jy: 如果 mid 值即为目标值, 直接返回
+            if nums[mid] == target:
+                return mid
 
+            # jy: 优先判断 nums[mid] 和 nums[high] 的值的大小;
+            #     如果 mid 小于 high, 表明后半部分严格升序, 此时判断
+            #     target 是否位于后半部分即可更新 low 或 high 值
+            if nums[mid] < nums[high]:
+                # jy: 当 target == nums[high] 时也更新 low, 使得不会错过正确值
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            # jy: 如果 mid 大于 high, 表明前半部分严格升序; 此时判断
+            #     target 是否位于前半部分区间即可更新 low 或 high 值
+            elif nums[mid] > nums[high]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            # jy: 此处逻辑即 nums[mid] == nums[high], 只有当有序 pivot 数组
+            #     中允许数值重复才有可能出现该情况 (如 0081)
+            else:
+                #high -= 1
+                high = mid - 1
         return -1
+
+
+    """
+解法 3: 还可以优先判断 mid 和 low 的值进行分类处理
+参见 0081 (Search-in-Rotated-Sorted-Array-II)
+    """
+    def search_v3(self, nums: List[int], target: int) -> int:
+        pass
+
+
 
 nums = [4, 5, 6, 7, 0, 1, 2]
 target = 0
-res = Solution().search(nums, target)
+res = Solution().search_v1(nums, target)
 print(nums, " === ", target, " === ", res)
 
 target = 3
-res = Solution().search(nums, target)
+res = Solution().search_v1(nums, target)
 print(nums, " === ", target, " === ", res)
 
 
 nums = [3, 4, 5, 6, 7, 0, 1, 2]
 target = 4
-res = Solution().search(nums, target)
+res = Solution().search_v1(nums, target)
 print(nums, " === ", target, " === ", res)
 
 
-print("==============【search_jy】=================")
 nums = [4, 5, 6, 7, 0, 1, 2]
 target = 0
-res = Solution().search_jy(nums, target)
+res = Solution().search_v2(nums, target)
 print(nums, " === ", target, " === ", res)
 
 target = 3
-res = Solution().search_jy(nums, target)
+res = Solution().search_v2(nums, target)
 print(nums, " === ", target, " === ", res)
 
 
 nums = [3, 4, 5, 6, 7, 0, 1, 2]
 target = 4
-res = Solution().search_jy(nums, target)
+res = Solution().search_v2(nums, target)
 print(nums, " === ", target, " === ", res)
-
-
-
-print("==============【081】=================")
-nums = [2, 5, 6, 0, 0, 1, 2]
-target = 0
-res = Solution().search_jy(nums, target)
-print(res)
-
-nums = [2, 5, 6, 0, 0, 1, 2]
-target = 3
-res = Solution().search_jy(nums, target)
-print(res)
-
 
--- leetcode_jy/jy_0501_1000/jy_0551_0600/jy_0556.py
+++ leetcode_jy/jy_0501_1000/jy_0551_0600/jy_0556.py
@@ -14,15 +14,16 @@ type_jy = "M"
 # jy: 记录该题的英文简称以及所属类别
 title_jy = "Next-Greater-Element-III(number)"
 # jy: 记录不同解法思路的关键词
-tag_jy = ""
+tag_jy = "理解题意后的技巧解法 | 参考 0031"
 
 
 """
-Given a positive integer n, find the smallest integer which has exactly the same digits existing
-in the integer n and is greater in value than n. If no such positive integer exists, return -1.
+Given a positive integer `n`, find the smallest integer which has exactly the
+same digits existing in the integer `n` and is greater in value than `n`. If
+no such positive integer exists, return -1.
 
-Note that the returned integer should fit in 32-bit integer, if there is a valid answer but it
-does not fit in 32-bit integer, return -1.
+Note that the returned integer should fit in 32-bit integer, if there is a 
+valid answer but it does not fit in 32-bit integer, return -1.
 
 
 Example 1:
@@ -41,51 +42,61 @@ Constraints:
 
 class Solution:
     """
-首先将数字转为数组, 然后从数组尾开始向左侧找到一段递增的子序列, 如果这段子序列最后结束于数组
-头(即数值已经由数字高到低排序组成), 说明不可能找到更小的数字, 返回 -1; 否则记当前子序列的起始
-位置为 i, 则有 nums[i-1] < nums[i], 为了找到比当前数字小的数, 可以在递减的子序列 nums[i:len(nums)]
-中找到一个比 nums[i-1] 大的数中最小的数, 将其和 nums[i-1] 互换, 最后反转 nums[i:len(nums], 进
-一步缩小数字;
+解法 1: 将数字转为数组, 然后从数组末尾开始从右到左找一段递增的子序列:
+a) 如果子序列最后结束于数组开头 (即数值已经由数字高到低排序组成), 说明
+   不可能找到更大的数字, 返回 -1; 可以以 n = 21 为例进行思考
+b) 如果子序列最后结束于下标 i (i != 0), 则有 nums[i-1] < nums[i], 为了
+   找到比当前数字大的数, 可以在递减的子序列 nums[i: len(nums)] (从右到
+   左递增, 因此从左到右是递减) 中找到一个比 nums[i-1] 大的数中最小的数,
+   将其和 nums[i-1] 互换, 最后反转 nums[i:len(nums)]; 可以以 12321 为例
+   进行思考
     """
-    def nextGreaterElement(self, n: int) -> int:
-        # jy: 将数字转为数组形式;
+    def nextGreaterElement_v1(self, n: int) -> int:
+        # jy: 将数字转为数组形式
         digits = list(str(n))
-        # jy: 从数组尾开始向左侧找到一段递增的子序列, 如果这段子序列最后结束于数组头, 直接返回 -1;
+        # jy: 从数组末尾开始从右向左找到一段递增的子序列, 如果这段子序
+        #     列最后结束于数组头, 直接返回 -1
         i = len(digits) - 1
         while i-1 >= 0 and digits[i] <= digits[i-1]:
             i -= 1
         if i == 0:
             return -1
 
-        # jy: 否则记当前子序列的起始位置为 i (i > 0), 有 digits[i-1] < digits[i];
+        # jy: 当前下标位置 i (i > 0) 满足 digits[i-1] < digits[i]
         j = i
-        # jy: 在递减的子序列 digits[i:len(digits)] 中找到比 digits[i-1] 大的数中的最小值 digits[j],
-        #    将其与 digits[i-1] 交换, 交换后 digits[i:len(digits)] 仍为递减子序列, 将其进行倒序,
-        #    使得进一步缩小数字;
+        # jy: 在递减的子序列 digits[i: len(digits)] 中找到比 digits[i-1]
+        #     大的数中的最小值 digits[j], 将其与 digits[i-1] 交换, 交换
+        #     后 digits[i:len(digits)] 仍为递减子序列, 将其进行倒序, 使得
+        #     进一步缩小数字
         while j + 1 < len(digits) and digits[j+1] > digits[i-1]:
             j += 1
-
         digits[i-1], digits[j] = digits[j], digits[i-1]
         digits[i:] = digits[i:][::-1]
-        # jy: 将结果列表拼接成数值;
+
+        # jy: 将结果列表拼接成数值
         result = int(''.join(digits))
-        # jy: 如果结果小于 2^31, 则返回该结果, 否则返回 -1;
+        # jy: 如果结果小于 2^31, 则返回该结果, 否则返回 -1
         return result if result < 1 << 31 else -1
 
 
 n = 12
 # Output: 21
-res = Solution().nextGreaterElement(n)
+res = Solution().nextGreaterElement_v1(n)
+print(res)
+
+n = 121
+# Output: 211
+res = Solution().nextGreaterElement_v1(n)
 print(res)
 
 n = 12321
-# Output: 21
-res = Solution().nextGreaterElement(n)
+# Output: 13122
+res = Solution().nextGreaterElement_v1(n)
 print(res)
 
 n = 21
 # Output: -1
-res = Solution().nextGreaterElement(n)
+res = Solution().nextGreaterElement_v1(n)
 print(res)
 
 
